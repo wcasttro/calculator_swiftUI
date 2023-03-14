72a67c236580a6fecccc982c5cc6562fd3a8ac64
wcasttro <wcastro.santos10@gmail.com>
2023-03-14 16:48:06 -0300

    Initial Commit

---
+++ calculator_swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator_swiftUI
+//
+//  Created by werthen castro on 14/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculator_swiftUI/calculator_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_swiftUIApp.swift
+//  calculator_swiftUI
+//
+//  Created by werthen castro on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct calculator_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
