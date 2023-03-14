45b690971640232e241234fd56bcce596b73331c
wcasttro <wcastro.santos10@gmail.com>
2023-03-14 17:38:33 -0300

    adicionando botões, com estrutura de linhas e colunas e campo de texto para o resultado

--- calculator_swiftUI/ContentView.swift
+++ calculator_swiftUI/ContentView.swift
@@ -8,9 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var result = "0";
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // VStack deixar os itens na vertical (uma Collumn)
+        // spacing remove o espaço entre seus filhos
+        VStack( alignment: .trailing, spacing: 0){
+            
+            Text(result).padding().font(.largeTitle).foregroundColor(Color.white)
+            HStack{ // HStack deixa os itens na horizontal (uma Row)
+                Button("7"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                Button("8"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                Button("9"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                Button("X"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                 .background(Color.orange)
+            }.foregroundColor(Color.white)
+            
+            HStack{
+                Button("4"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                Button("5"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                Button("6"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                Button("-"){
+                    
+                }.padding()
+                 .frame(maxWidth: .infinity)
+                 .background(Color.orange)
+            }.foregroundColor(Color.white)
+               HStack{
+                    Button("1"){
+                        
+                    }.padding()
+                     .frame(maxWidth: .infinity)
+                    Button("2"){
+                        
+                    }.padding()
+                     .frame(maxWidth: .infinity)
+                    Button("3"){
+                        
+                    }.padding()
+                     .frame(maxWidth: .infinity)
+                    Button("+"){
+                        
+                    }.padding()
+                     .frame(maxWidth: .infinity)
+                     .background(Color.orange)
+            }.foregroundColor(Color.white)
+            GeometryReader{ // Controla a gemetria (tamanhos da tela)
+                geometry in
+                HStack{
+                    Button("0"){
+                        
+                    }.padding()
+                        .frame(minWidth: geometry.size.width/2)
+                    Button(","){
+                        
+                    }.padding()
+                     .frame(maxWidth: .infinity)
+                   
+                    Button("="){
+                        
+                    }.padding()
+                     .frame(maxWidth: .infinity)
+                     .background(Color.orange)
+            }.foregroundColor(Color.white)
+                
+            }.frame(maxHeight: 52)
+            
+        }.frame(maxWidth: .infinity)
+         .background(Color.black)
+         .padding()
     }
 }
 
